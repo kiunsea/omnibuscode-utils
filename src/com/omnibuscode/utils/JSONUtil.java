5b3658b055f72e3bdb59192023c8c27c173ff013
lee.kiun <74360831+kiunsea@users.noreply.github.com>
2025-02-06 12:02:13 +0900

    version 2.1.3   > jackson library(json용) 버전 업그레이드     - 1.9.0 -> 2.16.1

--- src/com/omnibuscode/utils/JSONUtil.java
+++ src/com/omnibuscode/utils/JSONUtil.java
@@ -8,15 +8,16 @@ import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.JsonProcessingException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @author KIUNSEA
